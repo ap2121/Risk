3c0e62ba05f79ebdfc1500cb02d298fc47431dbd
Arthur Pate <arthurpate42@gmail.com>
2023-01-22 16:53:29 -0800

    added function to test for adjacent elements

--- main.js
+++ main.js
@@ -5,8 +5,9 @@ const buttonContainer = document.querySelectorAll('.buttons');
 const diceButton = document.getElementById('roll-dice');
 const attackButton = document.getElementById('attack-button');
 const moveButton = document.getElementById('move-button');
-const endButton = document.getElementById('move-button');
+const newButton = document.getElementById('new');
 //box elements
+const boxes = document.querySelectorAll('.boxes div')
 const box1 = document.getElementById("box-3");
 const box2 = document.getElementById("box-1");
 const box3 = document.getElementById("box-2");
@@ -14,15 +15,14 @@ const box4 = document.getElementById("box-4");
 const box5 = document.getElementById("box-5");
 const box6 = document.getElementById("box-7");
 const box7 = document.getElementById("box-6");
-const box8 = document.getElementById("box-9");
-const box9 = document.getElementById("box-8");
+const box8 = document.getElementById("box-8");
+const box9 = document.getElementById("box-9");
 const box10 = document.getElementById("box-10");
 //player box arrays
-const player1Boxes = [box1, box2, box3] 
-const neutralBoxes = [box4, box5, box6, box7];
-const player2Boxes = [box8, box9, box10];
+let player1Boxes = [box1, box2, box3] 
+let neutralBoxes = [box4, box5, box6, box7];
+let player2Boxes = [box8, box9, box10];
 //
-
 let player1Turn = false;
 let player2Turn = false;
 //
@@ -38,7 +38,7 @@ const rollDice = () => {
     }
 
 diceButton.addEventListener('click',rollDice)
-
+//who goes first function 
 const whoGoesFirst = (p1Dice, p2Dice) => {
     if(p1Dice === p2Dice) {
         textToggle.innerHTML = `Player 1 rolled ${p1Dice} and Player 2 rolled ${p2Dice}... Roll Again!`
@@ -54,3 +54,80 @@ const whoGoesFirst = (p1Dice, p2Dice) => {
         diceButton.removeEventListener('click', rollDice);
     }
 }
+
+const isAdjacent = (element1, element2) => {
+    if(element1 === box1 && element2 === box2 || element1 === box2 && element2 === box1) {
+        return true;
+    } else if(element1 === box1 && element2 === box3 || element1 === box3 && element2 === box1 ) {
+        return true;
+    } else if(element1 === box2 && element2 === box3 || element1 === box3 && element2 === box2 ) {
+        return true;
+    } else if(element1 === box1 && element2 === box5 || element1 === box5 && element2 === box1 ) {
+        return true;
+    } else if(element1 === box1 && element2 === box3 || element1 === box3 && element2 === box1 ) {
+        return true;
+    } else if(element1 === box5 && element2 === box6 || element1 === box6 && element2 === box5 ) {
+        return true;
+    } else if(element1 === box4 && element2 === box7 || element1 === box7 && element2 === box4 ) {
+        return true;
+    } else if(element1 === box5 && element2 === box4 || element1 === box4 && element2 === box5 ) {
+        return true;
+    } else if(element1 === box6 && element2 === box7 || element1 === box7 && element2 === box6 ) {
+        return true;
+    } else if(element1 === box4 && element2 === box6 || element1 === box6 && element2 === box4 ) {
+        return true;
+    } else if(element1 === box5 && element2 === box7 || element1 === box7 && element2 === box5 ) {
+        return true;
+    } else if(element1 === box7 && element2 === box9 ||  element1 === box9 && element2 === box7) {
+        return true;
+    } else if(element1 === box9 && element2 === box8 ||  element1 === box8 && element2 === box9) {
+        return true;
+    } else if(element1 === box9 && element2 === box10 ||  element1 === box10 && element2 === box9) {
+        return true;
+    } else if(element1 === box8 && element2 === box10 ||  element1 === box10 && element2 === box8) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+
+
+
+const testFunction = (e) => {
+    const clickedElement = e.target;
+    for(let box of boxes) {
+        box.style.border = "1px solid black";
+    }
+    testFunction2(clickedElement);
+}
+for(let box of boxes) {
+    box.addEventListener('click', testFunction)
+}
+const testFunction2 = (element) => {
+    
+    element.style.border = "5px solid orange";
+    for(let box of boxes) {
+        if(isAdjacent(element, box) === true) {
+            box.style.border = "5px solid blue";
+            
+        }
+    }
+}
+
+
+
+//new game function 
+const newGame = () => {
+    textToggle.innerHTML = "ROLL TO SEE WHO GOES FIRST!";
+    diceButton.addEventListener('click',rollDice);
+    player1Turn = false;
+    player2Turn = false;
+    player1Boxes = [box1, box2, box3];
+    neutralBoxes = [box4, box5, box6, box7];
+    player2Boxes = [box8, box9, box10];
+    for(let box of boxes) {
+        box.style.border = "1px solid black";
+    }
+}
+newButton.addEventListener('click', newGame);
