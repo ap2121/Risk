5c65d07209e4847adc0778c8d957a7c5843676b5
Arthur Pate <arthurpate42@gmail.com>
2023-01-25 06:20:47 -0800

    add soldier function addde to pass into event function

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
+//Text Toggle Elements
 const textToggle = document.getElementById('text-toggle');
-
+const title = document.getElementById('title')
+//////
 //button elements
 const buttonContainer = document.querySelectorAll('.buttons');
 const diceButton = document.getElementById('roll-dice');
@@ -7,6 +9,8 @@ const attackButton = document.getElementById('attack');
 const moveButton = document.getElementById('move');
 const newButton = document.getElementById('new');
 const endTurnButton = document.getElementById('end');
+const addButton = document.getElementById('add');
+/////
 //box elements
 const boxes = document.querySelectorAll('.boxes div')
 const box1 = document.getElementById("box-3");
@@ -19,23 +23,26 @@ const box7 = document.getElementById("box-6");
 const box8 = document.getElementById("box-8");
 const box9 = document.getElementById("box-9");
 const box10 = document.getElementById("box-10");
+/////
 //player box arrays
 let player1Boxes = [box1, box2, box3]; 
 let neutralBoxes = [box4, box5, box6, box7];
 let player2Boxes = [box8, box9, box10];
-//
+////
+//Global Toggle Variables
 let player1Turn = false;
 let player2Turn = false;
 let attack = false;
 let move = false;
+let add = false;
 let attackingScore = 0;
 let defendingScore = 0;
 let probabilityArray = [];
 let currentAS;
-let p1SCount = 1;
+let p1SCount = 2;
 let p2SCount = 1;
 //
-
+//Roll Dice Function
 const rollDice = () => {
     const p1DiceAmount = Math.floor(Math.random() * 6 + 1);
     const p2DiceAmount = Math.floor(Math.random () * 6 +1);
@@ -45,84 +52,121 @@ const rollDice = () => {
         console.log('Test')
     } 
     }
+/////
 
+//Dice button event handler
 diceButton.addEventListener('click',rollDice)
-
+///
+//Toggles Attack True and False
 const attackToggle = () => {
     if(attack === false) {
-        
         attack = true;
         attackButton.innerHTML = "End Attack"
-        console.log(attack)
+        
     } else if(attack === true) {
-    
         attack = false;
         attackButton.innerHTML = "Attack";
-        console.log(attack)
+       
     }
 }
+////
 
-attackButton.addEventListener('click', attackToggle);
-
+//Toggles Move True and False
 const moveToggle = () => {
     if(move === false) {
         move = true;
         moveButton.innerHTML = "Cancel";
-        console.log(move)
+        
         
     } else if(move === true) {
         move = false;
         moveButton.innerHTML = "Move";
-        console.log(move);
+        
     }
     
+
 }
 
-
-moveButton.addEventListener('click', moveToggle);
-
-
-
+//Gets and passes first element into function depending on Global Toggles
 const firstElement = (e) => {
     const clickedElement = e.target;
     currentAS = e.target;
     for(let box of boxes) {
         box.style.border = "1px solid black";
     }
-   if(attack === true) {
+   if(attack === true && move === false) {
     battleFunction(clickedElement);
     
     
-   } else if(attack === false) {
+   } else if(attack === false && move === false && add === false) {
     highlightAE(clickedElement);
-   }
+   } else if(add === true) {
+    addFunction(clickedElement);
+   } 
 }
+/////
+const addToggle = () => {
+    let currentSC;
+    if(player1Turn === true && player2Turn === false) {
+        currentSC = p1SCount;
+        if(player1Boxes.length <= player2Boxes.length) {
+            currentSC = 1;
+        } else if(player1Boxes.length > player2Boxes.length) {
+            currentSC = 2;
+        }
+    } 
+    else if(player2Turn === true && player1Turn === false) {
+        currentSC = p2SCount;
+        if(player2Boxes.length <= player1Boxes.length) {
+            currentSC = 1;
+        } else if(player2Boxes.length > player1Boxes) {
+            currentSC = 2;
+        }
+    }
+    if(add === false) {
+        add = true;
+        addButton.innerHTML = currentSC;
+        if(player1Turn === true & player2Turn === false) {
+            for(let box of player1Boxes) {
+                box.addEventListener('click', firstElement);
+            }
+        } else if(player2Turn === true && player1Turn === false) {
+            for(let box of player2Boxes) {
+                box.addEventListener('click', firstElement);  
+            }
+        }
+        console.log(add);
+    } else if(add === true) {
+        add = false;
+        addButton.innerHTML = 'Add Soldiers';
+        console.log(add);
 
-
+    }
+}
+/////
 //who goes first function 
 const whoGoesFirst = (p1Dice, p2Dice) => {
     if(p1Dice === p2Dice) {
         textToggle.innerHTML = `Player 1 rolled ${p1Dice} and Player 2 rolled ${p2Dice}... Roll Again!`
-           } else if(p1Dice > p2Dice) {
+           } 
+           else if(p1Dice > p2Dice) {
         textToggle.innerHTML = `Player 1 rolled ${p1Dice} and player 2 rolled ${p2Dice}... Player 1 goes first!`
         player1Turn = true;
-        for(let box of player1Boxes) {
-            box.addEventListener('click', firstElement);
-        }
+     
         diceButton.removeEventListener('click', rollDice);
-            
+        addButton.addEventListener('click', addToggle )  
     } 
     else if(p2Dice > p1Dice) {
         textToggle.innerHTML = `Player 2 rolled ${p2Dice} and Player 1 rolled ${p1Dice}... Player 2 goes first!`
         player2Turn = true;
-        for(let box of player2Boxes) {
-            box.addEventListener('click', firstElement)
-        }
+        
         diceButton.removeEventListener('click', rollDice);
+        addButton.addEventListener('click', addToggle);
     }
     
 }
-
+/////
+// Checks for adjacent elements for element selected
 const isAdjacent = (element1, element2) => {
     if(element1 === box1 && element2 === box2 || element1 === box2 && element2 === box1) {
         return true;
@@ -158,7 +202,8 @@ const isAdjacent = (element1, element2) => {
         return false;
     }
 }
-
+////
+//Checks for enemies of selected Element
 const isEnemy = (element1, element2) => {
     if(player1Boxes.includes(element1) && player2Boxes.includes(element2) || neutralBoxes.includes(element2)) {
         return true;
@@ -166,14 +211,8 @@ const isEnemy = (element1, element2) => {
         return true;
     } 
 }
-
-
-
-
-
-
-
-
+////
+//Logic for attacking, runs every time adjacent enemy is clicked.
 const battleLogic = (e) => { 
     attackingScore = parseInt(currentAS.innerHTML);
     defendingScore = parseInt(e.target.innerHTML); 
@@ -194,11 +233,9 @@ const battleLogic = (e) => {
         defendingArray = neutralBoxes;
     }
   }   
-  
-    
     if(attack === true && isEnemy(currentAS, e.target) === true && parseInt(currentAS.innerHTML) > 1){
         moveButton.removeEventListener('click', moveToggle);
-        
+        addButton.removeEventListener('click', addToggle)
         if(parseInt(currentAS.innerHTML) === parseInt(e.target.innerHTML)) {
             probabilityArray.push(0);
             probabilityArray.push(1);
@@ -273,7 +310,8 @@ const battleLogic = (e) => {
     
     probabilityArray = [];
 }
-
+////
+//Sets event handler for battle logic, first element selected passed in
 const battleFunction = (element) => {
     element.style.border = "5px solid yellow";
     for(let box of boxes) {
@@ -283,10 +321,31 @@ const battleFunction = (element) => {
             
     } 
     } 
-    
-   
-    
+    }
+// function ran if add soldiers is true;
+const addFunction = (element) => {
+   element.style.border = "5px solid purple";
+    let currentSC;
+    if(player1Turn === true && player2Turn === false) {
+        currentSC = p1SCount;
+        for(let box of player1Boxes) {
+            if(box !== element) {
+                box.style.border = "5px solid pink";
+            }
+            
+        }
+    } else if(player2Turn === true && player1Turn === false) {
+        currentSC = p2SCount;
+        for(let box of player2Boxes) {
+            if(box !== element) {
+                box.style.border = "5px solid pink";
+
+            }
+        }
+    }
 }
+//    
+//highlights adjacent friendlies and adjacent enemies 
 const highlightAE = (element) => {
     
     element.style.border = "5px solid orange";
@@ -299,7 +358,7 @@ const highlightAE = (element) => {
     }
     }
 }
-
+/////
 //end turn function
 
 // 
@@ -322,4 +381,4 @@ const newGame = () => {
    }
 }
 newButton.addEventListener('click', newGame);
-
+///
